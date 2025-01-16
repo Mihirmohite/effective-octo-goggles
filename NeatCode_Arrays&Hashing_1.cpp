6fc2e122d2e55d5794b6445416783d9e4bf778b2
Mihirmohite <101887795+Mihirmohite@users.noreply.github.com>
2025-01-16 13:49:06 +0530

    Create NeatCode_Arrays&Hashing_1.cpp
    CPP solution scripts for NeatCode, solved on LeetCode, NeatCode 150 problem 1.1

---
+++ NeatCode_Arrays&Hashing_1.cpp
@@ -0,0 +1,16 @@
+//CPP solution scripts for NeatCode, solved on LeetCode
+//Arrays & Hashing
+//Problem 1: Contains Duplicate
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for(int i = 0; i < nums.size() - 1; i++)
+        {   
+            if (nums[i] == nums[i+1])
+                return true;
+        }    return false;
+
+    }
+};
